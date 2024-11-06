8485a58a82b7fec37250c965d9e72bccf69b7f7f
Richard-wu-ICD2O1-02 <Richard.wu4@student.tdsb.on.ca>
2024-11-05 19:56:00 -0500

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -1,9 +1,5 @@
-
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
   print("Hello World")
-
 def q2():
   #Write Assignment code here
   print(1)
@@ -11,14 +7,13 @@ def q2():
   print(3)
   print(4)
   print(5)
-  
 def q3():
   #Write Assignment code here
-   print(" Help Me!")
+  print(" Help Me!")
 def q4():
   #Write Assignment code here
-print("3 x 3 = 9")
-#Do not alter anything after this line
+  print("3 x 3 = 9")
+
 q1()
 q2()
 q3()
